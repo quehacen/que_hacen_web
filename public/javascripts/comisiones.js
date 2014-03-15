86e087f213a598eca60091abd3cd0783abd15024
usuaro <pmedimara@gmail.com>
2014-03-15 15:01:16 +0100

    comisiones formato tabla, filtros en encabezado

--- public/javascripts/comisiones.js
+++ public/javascripts/comisiones.js
@@ -4,132 +4,79 @@
 		console.log('filtro');
 		return 'filtro';
 	}
-})(_);
+})(_);*/
+/*
+$(document).ready(function(){
+	$("th a").hover(
+	function() {
+		//alert('entro');
+		if($(this).hasClass("sel")==false){
+			$( this ).css("text-decoration","underline");
+		}else{
+			alert('tiene clase sel');
+		}
+	}, function() {
+		if($(this).hasClass("sel")==false)
+			$( this ).css("text-decoration","none");
+	});
+});*/
 
-function make_base_auth(user, password) {
-  var tok = user + ':' + password;
-  var hash = btoa(tok);
-  return "Basic " + hash;
-}*/
+$(document).ready(function(){
+	$().html('');
+	var srcImg="http://www.movidamovil.com/descargas/images/cmobile/loading.gif";
+        var img="<img src='"+srcImg+"' alt='cargando contenido'/>";
+        var div="<div style='min-height:400px;padding:50px 200px;width:100%'><p style='text-align:center'>"+img+"</p></div>";
+	$('.containerComisiones').html(div);
+});
 
-function tagsCom(sel){
-        var html='';
-        var hrefs= ["nombre","numDipus","fechaConst","legislativa","mixta","permanente"];  
-        var titulos= ["Nombre","nº diputados","fecha de constitución","Legislativas / No legislativas","Mixtas / No mixtas","Permanentes / no permanentes"];
-        for(var i=0;i<3;i++){
+
+
+function templateComisiones(sel){	
+	// Cabecera de la tabla
+	var template = '<table><tbody><tr>';
+        var hrefs= ["nombre","numDipus","fechaConst","legislativa","permanente","mixta"];  
+        var titulos= ["Nombre","Nº diputados","Fecha constitución","Legislativa","Permanente","Mixta"];
+        for(var i=0;i<6;i++){
                 if(hrefs[i]==sel){
-                        html=html+'<li class="tab active"><a href="#'+hrefs[i]+'">'+titulos[i]+'</a></li>';
+                        template+='<th><a class="sel" style="text-decoration:underline" href="#'+hrefs[i]+'">'+titulos[i]+'</a></th>';
                 }else{
-                        html=html+'<li class="tab"><a href="#'+hrefs[i]+'">'+titulos[i]+'</a></li>';
+                        template+='<th><a href="#'+hrefs[i]+'">'+titulos[i]+'</a></th>';
                 }
         }
-        html=html+'<li>|</li>';
-        for(var i=3;i<6;i++){
-                if(hrefs[i]==sel){
-                        html=html+'<li class="tab active"><a href="#'+hrefs[i]+'">'+titulos[i]+'</a></li>';
-                }else{
-                        html=html+'<li class="tab"><a href="#'+hrefs[i]+'">'+titulos[i]+'</a></li>';
-                }
-        }
-        return html;
+	template+='</tr>';
+	
+	
+	//Fila de cada comisión
+	template+='{{#data}}<tr><td><a href="/organo/{{normalized.url}}">{{nombre}}</a></td><td>{{numDiputados}}</td><td>{{constituida}}</td><td>{{legistxt}}</td><td>{{permtxt}}</td><td>{{mixtatxt}}</td></tr>{{/data}} </tbody></table>';
+	return template;
 }
-
 
 $(function(){
 	var Router = Backbone.Router.extend({
 		comisiones: null,
+		listo:null,
 		initialize: function(){
-			this.apiCall({q:'{"tipo":"^C"}',order:'{"nombre":1}'}, function(_data){
-				this.comisiones = _data;
-				//alert('we have comisiones!');
-			});
-		},
-		routes:{
-			//'' : 'nombreHandler',
-			'nombre' : 'nombreHandler',
-			'legislativa': 'legislativaHandler',
-			'mixta': 'mixtaHandler',
-			'permanente': 'permanenteHandler',
-			'numDipus':'numDipusHandler',
-			'fechaConst':'fechaConstHandler'
-		},
-
-		nombreHandler: function(){
-			//alert('HOME');
-			if(!this.comisiones){
-				setTimeout(this.nombreHandler, 500);
-				return;
-			}
-			var datos=[];
-			datos.data=this.comisiones
-			var template = "<table>{{#data}} <tr><td><a href='organo/{{normalized.url}}'>{{nombre}}</a></td></tr>{{/data}} </table>";
-			$('#tabsComisiones').html(tagsCom("nombre"));
-			$('.containerComisiones').html( Mustache.render(template, datos) );
-		},
-
-		legislativaHandler: function(){
-			console.log('legislativa');
-			if(!this.comisiones){
-				setTimeout(this.legislativaHandler, 500);
-				return;
-			}
-			var dataFiltered = _.groupBy(this.comisiones, function(comision){
-				return (comision.legis)?'legis':'nolegis'
-			});
-			var template = "<table> <tr> <th>Comisiones legislativas ({{legis.length}})</th> </tr> {{#legis}} <tr><td><a href='organo/{{normalized.url}}'>{{nombre}}</a></td></tr> {{/legis}}<tr> <th>Comisiones no legislativas ({{nolegis.length}})</th> </tr> {{#nolegis}} <tr><td><a href='organo/{{normalized.url}}'>{{nombre}}</a></td></tr> {{/nolegis}} </table>";
-			$('#tabsComisiones').html(tagsCom("legislativa"));
-			$('.containerComisiones').html( Mustache.render(template, dataFiltered) );
-		},
-
-		mixtaHandler: function(){
-			console.log('mixta');
-			if(!this.comisiones){
-				setTimeout(this.mixtaHandler, 500);
-				return;
-			}
-
-			var dataFiltered = _.groupBy(this.comisiones, function(comision){
-				return (comision.mixta)?'mixta':'nomixta'
-			});
-			var template = "<table> <tr> <th>Comisiones mixtas ({{mixta.length}})</th> </tr> {{#mixta}} <tr><td><a href='organo/{{normalized.url}}'>{{nombre}}</a></td></tr> {{/mixta}}<tr> <th>Comisiones no mixtas ({{nomixta.length}})</th> </tr> {{#nomixta}} <tr><td><a href='organo/{{normalized.url}}'>{{nombre}}</a></td></tr> {{/nomixta}} </table>";
-			$('#tabsComisiones').html(tagsCom("mixta"));
-			$('.containerComisiones').html( Mustache.render(template, dataFiltered) );
-		},
-
-		permanenteHandler: function(){
-			console.log('permanente');
-			if(!this.comisiones){
-				setTimeout(this.permanenteHandler, 500);
-				return;
-			}
-
-			var dataFiltered = _.groupBy(this.comisiones, function(comision){
-				return (comision.perm)?'perm':'noperm'
-			});
-			var template = "<table> <tr> <th>Comisiones permanentes ({{perm.length}})</th> </tr> {{#perm}} <tr><td><a href='organo/{{normalized.url}}'>{{nombre}}</a></td></tr> {{/perm}}<tr> <th>Comisiones no permanentes ({{noperm.length}})</th> </tr> {{#noperm}} <tr><td><a href='organo/{{normalized.url}}'>{{nombre}}</a></td></tr> {{/noperm}} </table>";
-			$('#tabsComisiones').html(tagsCom("permanente"));
-			$('.containerComisiones').html( Mustache.render(template, dataFiltered) );
-		},
-		
-		numDipusHandler: function(){
-			//console.log('numdipus');
-			/*this.apiDipusCall({q:'"cargos_congreso.tipoOrgano":"^C"',only:'["cargos_congreso"]'}, function(_data){
-				this.cargos_congreso= _data;
-				alert('we have data!');
-			});*/	
-			if(!this.comisiones){
-				setTimeout(this.numDipusHandler,500);
-				return;
-			}
-			$.ajax({
-                                url:'http://api.quehacenlosdiputados.net/diputados',
-				data: 'q={"cargos_congreso.tipoOrgano":"^C"}&only=["cargos_congreso"]'
-                        }).done(function(result){
-                                cargos_congreso=result;
+			$.when(
+				$.ajax('http://api.quehacenlosdiputados.net/organos?q={"tipo":"^C"}&order:{"nombre":1}'),
+				$.ajax('http://api.quehacenlosdiputados.net/diputados?q={"cargos_congreso.tipoOrgano":"^C"}&only:["cargos_congreso"}')
+			).done(function(_data,_data2){
+				// Añadimos campos que harán falta	
+				comisiones=_data[0];
 				_.each(comisiones,function(com){
-					//console.log('Comisión'+com.id+' iterando...');
+					if(com.legis==1){ com.legistxt="sí";
+					}else{ com.legistxt="no";}
+
+					if(com.perm==1){ com.permtxt="sí";}
+					else{ com.permtxt="no";}
+				
+					if(com.mixta==1){ com.mixtatxt="sí";
+					}else{ com.mixtatxt="no";}
+                        	});
+
+				// Añadimos nº de diputados
+				_.each(comisiones,function(com){
 					var num=0;
-                   			_.each(cargos_congreso,function(dipu){
+                   			_.each(_data2[0],function(dipu){
 						var tiene=false;
 						_.each(dipu.cargos_congreso, function(cargo){
 							//mejorar: con break
@@ -141,57 +88,115 @@ $(function(){
 					});
 					com.numDiputados=num;
               			});
-
-				var ordenadas=[];
-				var datos = _.sortBy(comisiones, function(com){ return com.numDiputados; });
-				datos.reverse();
-				ordenadas.data=datos;
-				//console.log(ordenadas);	
-				var template = "<table>{{#data}}<tr><td><a href='organo/{{normalized.url}}'>{{nombre}}</a> ({{numDiputados}} diputados)</td></tr> {{/data}} </table>";
-				$('#tabsComisiones').html(tagsCom("numDipus"));
-				$('.containerComisiones').html( Mustache.render(template, ordenadas) );
-                        });
-
+			}).done(function(){
+				listo=1;
+				//alert('ponemos listo a true');
+			});
 		},
+
+		routes:{
+			'' : 'nombreHandler',
+			'nombre' : 'nombreHandler',
+			'legislativa': 'legislativaHandler',
+			'mixta': 'mixtaHandler',
+			'permanente': 'permanenteHandler',
+			'numDipus':'numDipusHandler',
+			'fechaConst':'fechaConstHandler'
+		},
+
+		nombreHandler: function(){
+			if(!this.listo){
+				setTimeout(this.nombreHandler, 1000);
+				return;
+			}
+			var datos=[];
+			datos.data= _.sortBy(this.comisiones, function(com){ return com.nombre; });
+			var template = templateComisiones('nombre');
+			$('.containerComisiones').html( Mustache.render(template, datos) );
+		},
+
+		legislativaHandler: function(){
+			if(this.listo!=1){
+				console.log('reiniciamos');
+				setTimeout(this.legislativaHandler, 1000);
+				return 0;
+			}
+
+			var dataFiltered = _.groupBy(this.comisiones, function(comision){
+				return (comision.legis)?'legis':'nolegis'
+			});
+			var datos=[];
+			datos.data=_.union(dataFiltered['legis'],dataFiltered['nolegis']);
+			var template = templateComisiones('legislativa');
+			$('.containerComisiones').html( Mustache.render(template, datos) );
+		},
+
+		mixtaHandler: function(){
+			console.log('mixta');
+			if(!this.listo){
+				setTimeout(this.mixtaHandler, 1000);
+				return;
+			}
+
+			var dataFiltered = _.groupBy(this.comisiones, function(comision){
+				return (comision.mixta)?'mixta':'nomixta'
+			});
+			var datos=[];
+			datos.data=_.union(dataFiltered['mixta'],dataFiltered['nomixta']);
+			var template = templateComisiones('mixta');
+			$('.containerComisiones').html( Mustache.render(template, datos) );
+		},
+
+		permanenteHandler: function(){
+			console.log('permanente');
+			if(!this.listo){
+				setTimeout(this.permanenteHandler, 1000);
+				return;
+			}
+
+			var dataFiltered = _.groupBy(this.comisiones, function(comision){
+				return (comision.perm)?'perm':'noperm'
+			});
+			var datos=[];
+			datos.data=_.union(dataFiltered['perm'],dataFiltered['noperm']);
+			var template = templateComisiones('permanente');
+			$('.containerComisiones').html( Mustache.render(template, datos) );
+		},
+		
+		numDipusHandler: function(){
+			if(!this.listo){
+				setTimeout(this.numDipusHandler,1000);
+				return;
+			}
+			var datos = [];
+			datos.data=_.sortBy(this.comisiones, function(com){ return com.numDiputados; });
+			datos.data.reverse();
+			console.log(datos.data);
+			var template = templateComisiones('numDipus');
+			$('.containerComisiones').html( Mustache.render(template, datos) );
+		},
+
 		fechaConstHandler:function(){	
-			if(!this.comisiones){
-				setTimeout(this.fechaConstHandler,500);
+			if(!this.listo){
+				setTimeout(this.fechaConstHandler,1000);
 				return;
 			}
 		
-			var ordenadas=[];
-			var datos=_.sortBy(this.comisiones, function(com){
+			var datos=[];
+			datos.data=_.sortBy(this.comisiones, function(com){
 				var nums=com.constituida.split('/');
 				var fecha= new Date(nums[2],nums[1],nums[0]);  
 				var time=fecha.getTime();
-				//console.log(fecha.getTime());
 				return time;
-				//return fecha.getMilliseconds(); 
 			});
-			ordenadas.data=datos;
-			var template = "<table>{{#data}}<tr><td><a href='organo/{{normalized.url}}'>{{nombre}}</a> ({{constituida}})</td></tr> {{/data}} </table>";
-			$('#tabsComisiones').html(tagsCom("fechaConst"));
-			$('.containerComisiones').html( Mustache.render(template, ordenadas) );
+
+			var template = templateComisiones('fechaConst');
+			$('.containerComisiones').html( Mustache.render(template, datos) );
 		},
-		apiCall: function(_data, callback){
+
+		apiCall: function(col, _data, callback){
 			$.ajax({
-				url:'http://api.quehacenlosdiputados.net/organos',
-				//url:'http://localhost:3002/organos',	
-				//url:'http://quehacenlosdiputados.net:quehacenT3ST@api.quehacenlosdiputados.net/organos',
-				//username: 'quehacenlosdiputados.net',
-				//password: 'quehacenT3ST',
-				/*beforeSend: function (xhr){ 
-        			 xhr.setRequestHeader('Authorization', make_base_auth("quehacenlosdiputados.net","quehacenT3ST")); 
-    				},
-				headers: {Authorization: make_base_auth("quehacenlosdiputados.net","quehacenT3ST")},*/
-				data : _data
-			}).done(function(result){
-				callback(result);
-			});
-		},
-		apiDipusCall: function(_data, callback){
-			$.ajax({
-				url:'http://api.quehacenlosdiputados.net/diputados',
+				url:'http://api.quehacenlosdiputados.net/'+col,
 				data : _data
 			}).done(function(result){
 				callback(result);
@@ -201,4 +206,5 @@ $(function(){
 
 	var router = new Router();
 	Backbone.history.start();
+
 });
